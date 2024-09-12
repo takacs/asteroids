bd909299117c5918550930981e4793cc863626f7
takacs <davidetkcs@gmail.com>
2024-09-12 11:10:24 +0200

    add game loop

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
