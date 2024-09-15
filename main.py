b6f3c4e672268a172ead3ad747bfa17b3cd6dbfe
takacs <davidetkcs@gmail.com>
2024-09-15 11:12:29 +0200

    gitignore

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0, 0, 0))
         pygame.display.flip()
 
